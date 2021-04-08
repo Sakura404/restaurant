feb2fdec82c7faf52600c362dbb5e98d317e25b7
Sakura404 <380487608@qq.com>
2021-04-08 16:33:17 +0800

    menu and dashboard

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -1,3 +1,34 @@
 <template>
-<div>dashboard</div>
+	<v-container>
+		<v-row>
+			<v-col md=6  cols=12>
+				<v-card>
+					<v-sheet class=" mx-auto" elevation="12" max-width="calc(100% - 32px)" color='cyan'>
+						<v-sparkline color='white' line-width="2" padding="16" :labels='day()' smooth=10
+							:value='daysale'>
+						</v-sparkline>
+					</v-sheet>
+				</v-card>
+			</v-col>
+			{{day()}}
+		</v-row>
+	</v-container>
 </template>
+<script>
+	export default {
+		data() {
+			return {
+				daysale: [10, 20, 12,24,17,45,73, 30, 41],
+			}
+		},
+		methods: {
+			day() {
+				var x = new Array()
+				for (var i = 0; i < this.daysale.length; i++) {
+					x.push(i + 1)
+				}
+				return x
+			}
+		}
+	}
+</script>
