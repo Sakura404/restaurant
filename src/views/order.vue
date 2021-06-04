003c824b32fd85a38b3a9be7a8a3f7153739f3b2
Sakura404 <380487608@qq.com>
2021-06-04 17:05:55 +0800

    .

--- src/views/order.vue
+++ src/views/order.vue
@@ -1,18 +1,43 @@
 <template>
   <v-container>
-    <v-data-table :headers="headers" :items="foodmenu">
+    <v-data-table
+      hide-default-footer
+      dense
+      :headers="headers"
+      :items="foodmenu"
+    >
       <template v-slot:[`item.quantity`]="{ item }">
-        <v-btn icon>
-          <v-icon @click="quantityreduce(item)">mdi-arrow-left</v-icon></v-btn
+        <v-btn @click="quantityreduce(item)" icon>
+          <v-icon>mdi-minus</v-icon></v-btn
         >
         {{ item.quantity }}
-        <v-btn icon>
-          <v-icon @click="quantityadd(item)">mdi-arrow-right</v-icon></v-btn
+        <v-btn @click="quantityadd(item)" icon>
+          <v-icon>mdi-plus</v-icon></v-btn
         >
       </template>
-      
+      <template v-slot:[`item.total`]="{ item }">
+        {{ item.quantity * item.cost }}
+      </template>
+      <template v-slot:top>
+        <v-toolbar flat>
+          <v-toolbar-title>Order</v-toolbar-title>
+          <v-divider class="mx-4" inset vertical></v-divider>
+        </v-toolbar>
+      </template>
+      <template v-slot:footer>
+        <v-divider />
+        <v-row flat>
+          <v-col></v-col>
+          <v-spacer></v-spacer>
+          <v-col ><h3>total cost:{{ total }}￥</h3></v-col>
+        </v-row>
+        <v-row
+          ><v-col></v-col> <v-spacer></v-spacer
+          ><v-col cols="6"><v-btn text color="primary" width="70%">submit</v-btn></v-col
+          ><v-spacer></v-spacer><v-col></v-col
+        ></v-row>
+      </template>
     </v-data-table>
-    {{foodmenu}}
   </v-container>
 </template>
 <script>
@@ -30,17 +55,31 @@ export default {
           value: "name",
         },
         {
+          text: "cost(￥)",
+          value: "cost",
+        },
+        {
           text: "quantity",
           value: "quantity",
           sortable: false,
+          align: "center",
         },
         {
-          text: "cost(￥)",
-          value: "costs",
+          text: "total(￥)",
+          value: "total",
         },
       ],
       foodmenu: [],
     };
+  },
+  computed: {
+    total() {
+      var t = 0;
+      for (var a of this.foodmenu) {
+        t += a.quantity * a.cost;
+      }
+      return t;
+    },
   },
   methods: {
     foodget() {
@@ -57,13 +96,16 @@ export default {
         });
     },
     quantityadd(item) {
-       var index= this.foodmenu.indexOf(item)
-     console.log(index)
-     this.foodmenu[index].quantity++
-     console.log(this.foodmenu)
+      var index = this.foodmenu.indexOf(item);
+      item.quantity++;
+      this.foodmenu.splice(index, 1, item);
     },
     quantityreduce(item) {
-      if (item.quantity - 1 >= 0) item.quantity--;
+      if (item.quantity - 1 >= 0) {
+        var index = this.foodmenu.indexOf(item);
+        item.quantity--;
+        this.foodmenu.splice(index, 1, item);
+      }
     },
   },
   mounted() {
